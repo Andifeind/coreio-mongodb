7f85ce5dffaf1e13cba7c1865d1ae09643629ac7
Andifeind <andifeind@noname-media.com>
2017-02-25 08:25:59 +0100

    Fix test

--- tests/coreio-mongodb.spec.js
+++ tests/coreio-mongodb.spec.js
@@ -2,7 +2,7 @@
 
 let serviceTest = require('coreio-service-test');
 let CoreIO = require('coreio');
-let MongoDBService = require('../src/coreio-mongodb')(CoreIO);
+let MongoDBService = require('../lib/coreio-mongodb')(CoreIO);
 
 CoreIO.setConf('mongodb', 'mongodb://localhost:27017/coreio-test');
 serviceTest(MongoDBService);
